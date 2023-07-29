b7edcf0d81938617ee9f2657a456a4b4027fd495
yuchenhhu <73620514+yuchenhhu@users.noreply.github.com>
2023-07-28 21:07:00 -0700

    Update main.jl

--- mHealth-Study/main.jl
+++ mHealth-Study/main.jl
@@ -84,7 +84,7 @@ function genmod_on(n, capT, u = reward, burn = 50, init_states = 999)
         S[i, 8, 2] = S[i, 7, 1] # activity t-2
         
         eps = rand(d_er, 1)[1]
-        S[i, 1, 2] = (1 - beta[1]) * 100 + beta[1] * S[i, 1, 1] + beta[2] * S[i, 2, 1] + beta[3] * S[i, 2, 1] + beta[4] * S[i, 3, 1] + beta[5] * S[i, 3, 1] + beta[6] * A[i, 1] + beta[7] * A[i, 1] + eps + eps_id
+        S[i, 1, 2] = (1 - beta[1]) * 100 + beta[1] * (S[i, 1, 1]-eps_id) + beta[2] * S[i, 2, 1] + beta[3] * S[i, 2, 1] + beta[4] * S[i, 3, 1] + beta[5] * S[i, 3, 1] + beta[6] * A[i, 1] + beta[7] * A[i, 1] + eps + eps_id
         if S[i, 1, 2] > 250
             S[i, 1, 2] = 250  # prevent simulated glucose from going too high
         end
@@ -106,7 +106,7 @@ function genmod_on(n, capT, u = reward, burn = 50, init_states = 999)
                 S[i, 3, t] = S[i, 3, t] + max(rand(d_act_mod, 1)[1], 0)
             end
             eps = rand(d_er, 1)[1]
-            S[i, 1, t] = (1 - beta[1]) * 100 + beta[1] * S[i, 1, t - 1] + beta[2] * S[i, 2, t - 1] + beta[3] * S[i, 2, t - 2] + beta[4] * S[i, 3, t - 1] + beta[5] * S[i, 3, t - 2] + beta[6] * A[i, t - 1] + beta[7] * A[i, t - 2] + eps + eps_id
+            S[i, 1, t] = (1 - beta[1]) * 100 + beta[1] * (S[i, 1, t - 1]-eps_id) + beta[2] * S[i, 2, t - 1] + beta[3] * S[i, 2, t - 2] + beta[4] * S[i, 3, t - 1] + beta[5] * S[i, 3, t - 2] + beta[6] * A[i, t - 1] + beta[7] * A[i, t - 2] + eps + eps_id
             if S[i, 1, t] > 250
                 S[i, 1, t] = 250  # prevent simulated glucose from going too high
             end
@@ -157,7 +157,7 @@ function off_est(chain_on,n,t,k)
         est = mean(chain_r,dims=2)
         return est
     else
-        p = (chain_gl.>=110).*(chain_ex.<=100)
+        p = (chain_gl.>=125)
         all_one = fill(1,n,t)
         weights_ind = chain_a.*p./fill(0.3,n,t) .+
             (all_one-chain_a).*(all_one-p)./fill(0.7,n,t)
@@ -166,6 +166,7 @@ function off_est(chain_on,n,t,k)
         end
         chain_r_weighted = weights_roll.*chain_r[:,(k+1):end]
         est = mean(chain_r_weighted,dims=2)
+        #est = ifelse.(weights_roll.==0, 0., sum(chain_r_weighted,dims=2)./sum(weights_roll,dims=2))
         return est
     end
 end
@@ -184,16 +185,16 @@ function all_k(n,t,k_all,ey_off)
     # k_max
     results = off_est(chain_on,n,t,k_all[1])
     ey_off_est[1] = mean(results); sd_est[1] = std(results)
-    analyt_low[1] = ey_off_est[1] - 1.96*sd_est[1]/sqrt(n)
-    analyt_high[1] = ey_off_est[1] + 1.96*sd_est[1]/sqrt(n)
+    analyt_low[1] = ey_off_est[1] - sd_est[1]/sqrt(n)
+    analyt_high[1] = ey_off_est[1] + sd_est[1]/sqrt(n)
     slope_low = analyt_low[1]; slope_high = analyt_high[1]
     
     # k < k_max
     for i = 2:length(k_all)
         results = off_est(chain_on,n,t,k_all[i])
         ey_off_est[i] = mean(results); sd_est[i] = std(results)
-        analyt_low[i] = ey_off_est[i] - 1.96*sd_est[i]/sqrt(n)
-        analyt_high[i] = ey_off_est[i] + 1.96*sd_est[i]/sqrt(n)
+        analyt_low[i] = ey_off_est[i] - sd_est[i]/sqrt(n)
+        analyt_high[i] = ey_off_est[i] + sd_est[i]/sqrt(n)
         if (((analyt_high[i]<slope_low)|(analyt_low[i]>slope_high))&(marker==false))
             analyt_mse = (ey_off_est[i-1]-ey_off)^2
             marker = true
@@ -216,11 +217,11 @@ seed_num = rand(1:1000000, 1)[1]
 Random.seed!(seed_num)
 
 # start simulation
-ey_off = -1.69938
-k_all = -1:6
-t_all = collect(50:50:150)
-n_all = collect(50:50:150)
-B = 30
+ey_off = -0.68761
+k_all = -1:12
+t = 240
+n_all = collect(10:5:30).^2
+B = 1000
 
 res1 = zeros(0)
 res2 = zeros(0)
@@ -228,13 +229,11 @@ res3 = zeros(0)
 res4 = zeros(0)
 for b in 1:B
     for n in n_all
-        for t in t_all
-            results = all_k(n,t,k_all,ey_off)
-            append!(res1, results[1])
-            append!(res2, results[2])
-            append!(res3, results[3])
-            append!(res4, results[4])
-        end
+		results = all_k(n,t,k_all,ey_off)
+		append!(res1, results[1])
+		append!(res2, results[2])
+		append!(res3, results[3])
+		append!(res4, results[4])
     end
 end
 
